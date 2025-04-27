8537c0619ee73edfa97b56681693ccf4d6aa7351
Bor Praznik <praznik.bor@gmail.com>
2025-04-27 21:27:01 +0200

    give it all

---
+++ dist/cjs/client.js
@@ -0,0 +1,26 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var client = require('./client-a22d7500.js');
+var client$1 = require('./client-76dec77b.js');
+require('nanoid/non-secure');
+require('./Debug-9d141c06.js');
+require('./turn-order-b2ff8740.js');
+require('immer');
+require('./plugin-random-7425844d.js');
+require('lodash.isplainobject');
+require('flatted');
+require('./reducer-16eec232.js');
+require('rfc6902');
+require('./ai-e0e8a768.js');
+require('setimmediate');
+require('redux');
+require('./initialize-267fcd69.js');
+require('./transport-b1874dfa.js');
+
+
+
+exports.Client = client.Client;
+exports.LobbyClient = client$1.LobbyClient;
+exports.LobbyClientError = client$1.LobbyClientError;
---
+++ dist/esm/client.js
@@ -0,0 +1,16 @@
+export { C as Client } from './client-2e653027.js';
+export { L as LobbyClient, a as LobbyClientError } from './client-5f57c3f2.js';
+import 'nanoid/non-secure';
+import './Debug-0141fe2d.js';
+import './turn-order-376d315e.js';
+import 'immer';
+import './plugin-random-087f861e.js';
+import 'lodash.isplainobject';
+import 'flatted';
+import './reducer-c46da7e5.js';
+import 'rfc6902';
+import './ai-5c06e761.js';
+import 'setimmediate';
+import 'redux';
+import './initialize-11d626ca.js';
+import './transport-ce07b771.js';
